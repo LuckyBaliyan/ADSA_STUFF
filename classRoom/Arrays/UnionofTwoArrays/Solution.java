c0e15cf8ab8552ef499bafde571a8ca3b9d6e6db
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-10-23 21:38:27 +0530

    Day(14) leetcode 283 move zeros to end and Union of Two arrays

---
+++ classRoom/Arrays/UnionofTwoArrays/Solution.java
@@ -0,0 +1,98 @@
+package leetcode.Arrays2.UnionofTwoArrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Stack;
+
+public class Solution {
+    static int [] UnionofTwoArrays(int [] n1,int [] n2){
+        HashSet<Integer> st = new HashSet<>();
+        
+        for(int i:n1){
+            st.add(i);
+        }
+
+        for(int a:n2){
+            st.add(a);
+        }
+
+        int [] unionArr = new int [st.size()];
+        int i = 0;
+
+        for(int val: st){
+          unionArr[i++] = val;
+        }
+
+        Arrays.sort(unionArr);
+        return unionArr;
+    }
+
+    static int [] unionOptimal(int [] n1,int [] n2){
+        int l1 = n1.length;
+        int l2 = n2.length;
+
+        Stack<Integer> st = new Stack<>();
+
+        int j = 0;
+        int i = 0;
+
+        while(i<l1 && j<l2){
+            if(n1[i] <= n2[j]){
+                if(st.size() == 0 || st.peek() != n1[i]){
+                    st.push(n1[i]);
+                }
+                i++;
+            }
+            else{
+                if(st.size() == 0 || st.peek() != n2[j]){
+                    st.push(n2[j]);
+                }
+                j++;
+            }
+        }
+
+        while(i<l1){
+            if(st.size() == 0 || st.peek() != n1[i]){
+                st.push(n1[i]);
+            }
+            i++;
+        }
+
+        while(j<l2){
+            if(st.size() == 0 || st.peek() != n2[j]){
+                st.push(n2[j]);
+            }
+            j++;
+        }
+
+        int [] arr = new int[st.size()];
+        int k = st.size() - 1;
+
+        while(!st.isEmpty()){
+            arr[k--] = st.pop();
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int [] n1 = {0,2,3,4,5,6};
+        int [] n2 = {1,2,7};
+
+        int [] nums1 = {3, 4, 6, 7, 9, 9}, nums2 = {1, 5, 7, 8, 8};
+
+        int [] arr = UnionofTwoArrays(nums1,nums2);
+
+        for(int e:arr){
+            System.out.print(e+" ");
+        }
+
+        System.out.println();
+
+        int [] arr2 = unionOptimal(n1, n2);
+        
+        for (int i : arr2) {
+            System.out.print(i+" ");
+        }
+    }
+}
---
+++ leetcode/Arrays2/leetcode_283/Solution.java
@@ -0,0 +1,90 @@
+package leetcode.Arrays2.leetcode_283;
+
+/**
+ * 
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order
+ *  of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+ */
+
+public class Solution {
+    static void placeZeros(int [] nums){
+        int n = nums.length;
+
+        if(n<=1) return;
+
+        int i = 0;
+        int j = 1;
+
+        while(j<n){
+            if(nums[i] == 0 && nums[j] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+                i++;
+                j++;
+            }
+            else if(nums[i] == 0 && nums[j] == 0){
+                j++;
+            }
+            else{
+                i++;
+                j++;
+            }
+        }
+    }
+
+    static void placethemSmartly(int [] arr){
+        int n = arr.length;
+
+        if(n<=1) return;
+
+        //Overwritting all the non-zero values first
+ 
+        int i = 0;
+        for(int a:  arr){
+            if(a != 0){
+                arr[i++] = a;
+            }
+        }
+
+        //Now until i < n that means we have some space left to fill with zeros
+
+        while(i<n){
+            arr[i++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {0,1,0,3,0,0,3,0,0,2};
+        placeZeros(arr);
+
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+
+        int [] arr2 = {1,2,3,0,0,0,0,0,0,0,1};
+        placethemSmartly(arr2);
+
+        System.out.println();
+
+        for(int e:arr2){
+            System.out.print(e+" ");
+        }
+    }
+    
+}
