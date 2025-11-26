7f135896ea3f77675d6ccddd22c8b2fa9b64f8c0
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-11-26 12:49:36 +0530

    Permutation of Strings EliteBatch Question with Brute Force Solution

---
+++ gfg/Recursion/Permutation_String/Solution.java
@@ -0,0 +1,31 @@
+package gfg.Recursion.Permutation_String;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+class Solution {
+    public static ArrayList<String> findPermutation(String s) {
+        // Code here
+        HashSet<String> st = new HashSet<>();
+        solve(s,"",st);
+        return new ArrayList<>(st);
+    }
+    
+    public static void solve(String s,String curr,HashSet<String> st){
+        if(s.length() == 0){
+            st.add(curr);
+            return;
+        }
+        
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            String rem = s.substring(0,i)+s.substring(i+1);
+            
+            solve(rem,curr + ch,st);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findPermutation("ABC"));
+    }
+}
