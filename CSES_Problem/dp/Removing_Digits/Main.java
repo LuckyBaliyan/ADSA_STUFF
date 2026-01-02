df6ebddbde5e7696e117d984a548a9f214900c54
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-02 15:58:13 +0530

    CSES RemovingDigits tabulation Approach done

--- CSES_Problem/dp/Removing_Digits/Main.java
+++ CSES_Problem/dp/Removing_Digits/Main.java
@@ -1,15 +1,57 @@
-package CSES_Problem.dp.Removing_Digits;
+//package CSES_Problem.dp.Removing_Digits;
 
+//import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public static int solve(int n,int [] dp){
+        if(n == 0)return 0;
+
+        if(dp[n] != -1)return dp[n];
+
+        int temp = n;
+        int ans = Integer.MAX_VALUE;
+
+        while(temp > 0){
+            int d = temp % 10;
+            temp /= 10;
+
+            if(d != 0){
+                ans = Math.min(ans,1 + solve(n - d, dp));
+            }
+        }
+
+        return dp[n] =  ans;
+    }
+
+    public static int solveTab(int n){
+        int [] dp = new int [n+1];
+
+        dp[0] = 0;
+
+        for(int i = 1;i<=n;i++){
+            int temp = i;
+            dp[i] = Integer.MAX_VALUE;
+
+            while (temp > 0) {
+                int dig = temp % 10;
+                temp = temp / 10;
+
+                if(dig != 0){
+                    dp[i] = Math.min(dp[i], 1 + dp[i - dig]);
+                }
+            }
+        }
+
+        return dp[n];
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
         //int copy = n;
-        int ways = 0;
+        //int ways = 0;
 
         /* 
         while(copy > 0){
@@ -28,6 +70,7 @@ public class Main {
         }
         */
 
+        /* 
         while( n > 0){
             int temp = n;
             int max = 0;
@@ -41,7 +84,11 @@ public class Main {
             n -= max;
             ways++;
         }
+        */
+        
+        //int [] dp = new int [n + 1];
+        //Arrays.fill(dp,-1);
 
-        System.out.println(ways);
+        System.out.println(solveTab(n));
     }
 }
