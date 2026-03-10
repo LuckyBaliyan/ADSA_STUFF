97679cc3daee5a077c4011d3ad7922adefeb5e4f
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-10 12:55:30 +0530

    SegmentTree Sum and updateQuery

--- RangeQueries/Concepts/SegmentTree/Main.java
+++ RangeQueries/Concepts/SegmentTree/Main.java
@@ -19,13 +19,36 @@ public class Main {
 
         seg[indx] = seg[2*indx+1] + seg[2*indx+2];
     }
+
+    public static long sumQuery(int ql,int qr,int idx,int l,int r){
+        //if query range  completely outside the given range
+        if(r < ql || l > qr)return 0;
+
+        //if completely inside in the queryRange
+        if(l >= ql && r <= qr)return seg[idx];
+
+        int mid = l+(r-l)/2;
+        long left = sumQuery(ql, qr, 2*idx+1, l, mid);
+        long right = sumQuery(ql, qr, 2*idx+2, mid+1, r);
+
+        return left + right;
+    }
     public static void main(String[] args) {
         int n = 6;
+        /*
+        4n is always an upperBound for all sizes so we just use it to work for every 
+        given i/p
+        */
         seg = new long [4*n];
 
         build_st(0, 0, n-1);
 
         for(long s:seg)System.out.print(s+" ");
 
+        System.out.println();
+
+        //passing a range sume query
+        long res = sumQuery(2, 4, 0, 0, n-1);
+        System.out.println(res);
     }
 }
