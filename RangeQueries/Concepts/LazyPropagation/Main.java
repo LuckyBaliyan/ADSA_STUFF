a136afd57f06c42cfd641806830f04862e614ea7
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-22 22:40:46 +0530

    Added Query with lazyLoading

--- RangeQueries/Concepts/LazyPropagation/Main.java
+++ RangeQueries/Concepts/LazyPropagation/Main.java
@@ -51,6 +51,24 @@ class Main{
 
         seg[idx] = seg[2*idx+1] + seg[2*idx+2];
     }
+
+    public static int query(int idx,int l,int r,int ql,int qr){
+        if(lazy[idx]!= 0){
+            seg[idx] += lazy[idx] * (r - l + 1);
+            if(l!= r){
+                lazy[2*idx+1] += lazy[idx];
+                lazy[2*idx+2] += lazy[idx];
+            }
+
+            lazy[idx] = 0;
+        }
+
+        if(r < ql || l > qr)return 0;
+        if(ql<= l && r <= qr)return seg[idx];
+
+        int mid = l+(r-l)/2;
+        return query(2*idx+1, l, mid, ql, qr) + query(2*idx + 2, mid + 1, r, ql, qr);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -63,6 +81,7 @@ class Main{
         for(int i = 0;i<n;i++)arr[i] = sc.nextInt();
 
         build_st(0,0,n-1);
+        int res = 0;
 
         for(int i = 0;i<m;i++){
             int l = sc.nextInt();
@@ -70,11 +89,17 @@ class Main{
             int val = sc.nextInt();
 
             update_st(0,0,n-1,l,r,val);
+            //random query
+
+            res += query(0, 0, n-1, l, r);
         }
 
         for(int x:seg)System.out.print(x+" ");
         System.out.println();
         for(int x:lazy)System.out.print(x+" ");
+        System.out.println();
+        
+        System.out.println(res);
 
         sc.close();
     }
