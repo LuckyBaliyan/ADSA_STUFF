4b595e2ceaac15997d8752372ab346d534e8fd1b
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-03 22:00:11 +0530

    CF Boy or Girl

---
+++ CodeForces/ConceptsRevision/BoyOrGirl/Main.java
@@ -0,0 +1,26 @@
+//package CodeForces.ConceptsRevision.BoyOrGirl;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String Pattern = sc.nextLine();
+        int [] freq = new int [26];
+
+        for(int i = 0;i<Pattern.length();i++){
+            freq[Pattern.charAt(i) - 'a']++;
+        }
+
+        int count = 0;
+        for(int i = 0;i<26;i++){
+            if(freq[i] > 0)count++;
+        }
+
+        String res = (count % 2 == 0) ? "CHAT WITH HER!":"IGNORE HIM!";
+        System.out.println(res);
+
+        sc.close();
+    }
+}
